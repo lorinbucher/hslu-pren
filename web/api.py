ab255557959b5c852c9a24eaace90ef878f3d80c
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2023-10-26 08:57:44 +0200

    api: fix wrong server address

--- web/api.py
+++ web/api.py
@@ -5,8 +5,8 @@ import requests
 
 import shared.config as app_config
 
-SERVER_URL = 'http://188.192.48.168:5000'
-TEAM_NR = "03"
+SERVER_URL = 'http://18.192.48.168:5000'
+TEAM_NR = '03'
 
 
 class CubeApi:
@@ -17,8 +17,8 @@ class CubeApi:
 
     def get_availability(self) -> str:
         """Sends a GET request to the availability endpoint"""
-        url = SERVER_URL + "/cubes"
-        self._logger.info('send availability request: GET %s', url)
+        url = SERVER_URL + '/cubes'
+        self._logger.info("send availability request: GET %s", url)
         try:
             response = requests.get(url=url, timeout=5)
             self._logger.info("availability response - status: %s, text: %s", response.status_code, response.text)
@@ -29,8 +29,8 @@ class CubeApi:
 
     def post_cube_config(self, config: dict[str, str]) -> bool:
         """Sends a POST request to the cubes endpoint"""
-        url = SERVER_URL + "/cubes/team" + TEAM_NR
-        self._logger.info('send cube config request: POST %s %s', url, config)
+        url = SERVER_URL + '/cubes/team' + TEAM_NR
+        self._logger.info("send cube config request: POST %s %s", url, config)
         try:
             response = requests.post(url=url, json=config, timeout=5)
             self._logger.info("cube config response - status: %s, text: %s", response.status_code, response.text)
