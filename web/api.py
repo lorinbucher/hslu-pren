43824f861791005a2b1e07f45b96c6dd1c79d6de
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2023-10-29 16:27:15 +0100

    api: fix response handling

--- web/api.py
+++ web/api.py
@@ -14,17 +14,17 @@ class CubeApi:
         self._address = app_config.server_api_address
         self._team_nr = app_config.auth_team_nr
 
-    def get_availability(self) -> str:
+    def get_availability(self) -> bool:
         """Sends a GET request to the availability endpoint"""
         url = 'http://' + self._address + '/cubes'
         self._logger.info("send availability request: GET %s", url)
         try:
             response = requests.get(url=url, timeout=5)
             self._logger.info("availability response - status: %s, text: %s", response.status_code, response.text)
-            return response.text
+            return 200 <= response.status_code <= 299
         except requests.exceptions.RequestException as error:
             self._logger.error("request failed: %s", error)
-            return ""
+            return False
 
     def post_cube_config(self, config: dict[str, str]) -> bool:
         """Sends a POST request to the cubes endpoint"""
@@ -32,8 +32,8 @@ class CubeApi:
         self._logger.info("send cube config request: POST %s %s", url, config)
         try:
             response = requests.post(url=url, json=config, timeout=5)
-            self._logger.info("cube config response - status: %s, text: %s", response.status_code, response.text)
-            return response.status_code % 100 == 2
+            self._logger.info("cube config response - status: %s, text: %s", response.status_code, response.json())
+            return 200 <= response.status_code <= 299
         except requests.exceptions.RequestException as error:
             self._logger.error("request failed: %s", error)
             return False
