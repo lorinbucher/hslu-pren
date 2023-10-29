4978967a1d8642bccef9caee19506bf737a50ff9
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2023-10-29 16:39:42 +0100

    api: use token for authentication

--- web/api.py
+++ web/api.py
@@ -13,6 +13,7 @@ class CubeApi:
         self._logger = logging.getLogger('web.cube_api')
         self._address = app_config.server_api_address
         self._team_nr = app_config.auth_team_nr
+        self._auth_token = app_config.auth_token
 
     def get_availability(self) -> bool:
         """Sends a GET request to the availability endpoint"""
@@ -26,12 +27,13 @@ class CubeApi:
             self._logger.error("request failed: %s", error)
             return False
 
-    def post_cube_config(self, config: dict[str, str]) -> bool:
+    def post_cube_config(self, cube_config: dict[str, str]) -> bool:
         """Sends a POST request to the cubes endpoint"""
         url = 'http://' + self._address + '/cubes/team' + self._team_nr
-        self._logger.info("send cube config request: POST %s %s", url, config)
+        headers = {'Accept': 'application/json', 'Authorization': f'Bearer {self._auth_token}'}
+        self._logger.info("send cube config request: POST %s %s", url, cube_config)
         try:
-            response = requests.post(url=url, json=config, timeout=5)
+            response = requests.post(url=url, headers=headers, json=cube_config, timeout=5)
             self._logger.info("cube config response - status: %s, text: %s", response.status_code, response.json())
             return 200 <= response.status_code <= 299
         except requests.exceptions.RequestException as error:
