ec42e16fd76654b7194312ee49e3f5d24853f8de
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2024-06-04 22:52:36 +0200

    static: update button colors, display slider value in settings

--- static/script.js
+++ static/script.js
@@ -15,3 +15,19 @@ settingsButton.addEventListener('click', () => {
     settingsPage.classList.add('active');
     homePage.classList.remove('active');
 });
+
+// Settings
+const confidenceSlider = document.getElementById("confidence");
+const recognitionTimeoutSlider = document.getElementById("recognition-timeout");
+const confidenceValue = document.getElementById("confidence-value");
+const recognitionTimeoutValue = document.getElementById("recognition-timeout-value");
+
+updateSliderValue(confidenceSlider, confidenceValue, " frames");
+updateSliderValue(recognitionTimeoutSlider, recognitionTimeoutValue, "s");
+
+// Functions
+function updateSliderValue(slider, valueDisplay, unit = "") {
+    slider.addEventListener("input", (event) => {
+        valueDisplay.textContent = event.target.value + unit;
+    });
+}
