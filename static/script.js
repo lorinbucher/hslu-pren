48672bcbe4b9f3c36d7391c08110009511a8f38b
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2024-06-04 22:25:53 +0200

    static: update website layout, data and design

--- static/script.js
+++ static/script.js
@@ -1,25 +1,17 @@
-function toggleSettings() {
-    var container = document.querySelector('.container');
-    var settingsButtons = document.querySelector('.settings-buttons');
-    var settings = document.querySelector('.settings');
-    var controls = document.querySelector('.controls');
+"use strict"
 
-    if (container.style.display === 'none') {
-        container.style.display = 'block';
-        settings.style.display = 'none';
-        settingsButtons.style.display = 'none';
-        controls.style.display = 'flex';
-    } else {
-        container.style.display = 'none';
-        settings.style.display = 'block';
-        settingsButtons.style.display = 'flex';
-        controls.style.display = 'none';
-    }
-}
+// Navigation
+const homeButton = document.getElementById('btn-home');
+const settingsButton = document.getElementById('btn-settings');
+const homePage = document.getElementById('home');
+const settingsPage = document.getElementById('settings');
 
-function changeColor(button) {
-    var colors = ["red", "yellow", "blue", "white"];
-    var currentIndex = colors.indexOf(button.style.backgroundColor);
-    var newIndex = (currentIndex + 1) % colors.length;
-    button.style.backgroundColor = colors[newIndex];
-}
+homeButton.addEventListener('click', () => {
+    homePage.classList.add('active');
+    settingsPage.classList.remove('active');
+});
+
+settingsButton.addEventListener('click', () => {
+    settingsPage.classList.add('active');
+    homePage.classList.remove('active');
+});
