946ef879fe86b6950d439500654b747f05d10203
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2024-06-29 15:21:02 +0200

    fix website not centered correctly on the touch display

--- chromium_start.sh
+++ chromium_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Auto start the chromium browser fullscreen and in kiosk mode.
 
-/usr/bin/chromium-browser --start-fullscreen --kiosk http://localhost:5000/index.html
+/usr/bin/chromium-browser --app=http://localhost:5000/index.html --start-fullscreen --kiosk
